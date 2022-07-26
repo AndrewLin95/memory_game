4d3f3936cd9e8ffd57025b6ab9fffa2e6fca2581
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-26 19:09:32 -0400

    minor update to structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Display from './components/Display';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header/>
+      <Display/>
     </>
   );
 }
