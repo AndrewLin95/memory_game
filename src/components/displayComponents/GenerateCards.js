4dcaf59b32c0ae4623b7035c1e0c2ee59aea427b
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-29 01:19:24 -0400

    adjusted card size and positioning of images

--- src/components/displayComponents/GenerateCards.js
+++ src/components/displayComponents/GenerateCards.js
@@ -8,7 +8,6 @@ const GenerateCards = ( { cards, handlePlayerAction } ) => {
                 return (
                     <div key={cards.key} id={cards.key} className='cardDiv' onClick={()=>{handlePlayerAction(cards.key)}}>
                         <img src={require(`../../images/${cards.image}.png`)} alt="card visual" width={"100%"} height={"auto"}/>
-                        <div></div>
                     </div>
                 )
             })
