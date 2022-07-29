20e4ecc80c4d6827ae35552f7061289fbb79f679
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-29 00:00:16 -0400

    added footer that links to github project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Display from './components/Display';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header/>
       <Display/>
+      <Footer/>
     </>
   );
 }
