084d6c429ca6e8755bcdd1eacc98060636ac0125
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-23 18:24:14 -0400

    gh actions test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component{
         return(
             <div id='footer'>
                 <a href='https://github.com/AndrewLin95/memory_game' target="_blank" rel='noreferrer'>{githubIcon}</a>
-                <div>Test for GH Actions Deploy2</div>
+                <div>Test for GH Actions Deploy with npm</div>
             </div>
         )
     }
